c55dfa874dd6f35ea851e2c056275246ea5b2cba
Thangmai02 <maivanthang10a6@gmail.com>
2024-10-01 15:52:14 +0700

    backend

--- src/main/java/org/example/c11_sping/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.c11_sping.repository;
-
-import org.example.c11_sping.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
